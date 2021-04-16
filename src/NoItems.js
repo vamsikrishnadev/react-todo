4e96885d29ce603bc2eb21fd5295946629ef547d
vamsikrishnadev <vamsikrishnadev@gmail.com>
2021-04-16 16:49:53 +0530

    eqeqeq update

--- src/NoItems.js
+++ src/NoItems.js
@@ -4,7 +4,7 @@ export default class NoItems extends react.Component
 {
     render()
     { 
-        if (this.props.itemCount==0)
+        if (this.props.itemCount===0)
         return <div>List is empty</div>
         else
         return ""
